6b0f107c9566a301251140d8f38b43b6a5964461
chrisheckler <hecklerchris@hotmail.com>
2015-12-03 23:09:57 -0800

    attempting to add about page with urls & views

--- rango/views.py
+++ rango/views.py
@@ -6,3 +6,6 @@ def home(request):
     index_html = 'index.html'
     return render(request, index_html)
 
+def about(request):
+    about_html = 'about.html'
+    return render(request, about_html)
