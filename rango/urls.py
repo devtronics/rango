af3b49f871f4e067cc2c6999127ef87f50d50bf0
chrisheckler <hecklerchris@hotmail.com>
2015-12-05 15:26:02 -0800

    add more images, media server good to go

--- rango/urls.py
+++ rango/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import *
 from django.contrib import admin
 from rango import views
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     # Examples:
@@ -11,12 +12,11 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^about', 'rango.views.about', name='about'),
     url(r'^contact', 'rango.views.contact', name='contact'),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += patterns(
-            'django.views.static',
-            (r'^media/(?P<path>, *)',
-            'serve',
-            {'document_root': settings.MEDIA_ROOT}),  )
+    urlpatterns += patterns('django.views.static', (r'^media/(?P<path>,*)', 'serve', {'document_root': settings.MEDIA_ROOT}), )
 
