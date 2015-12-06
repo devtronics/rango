eacdeb411741beb276e91b483657aa97cb552e8d
chrisheckler <hecklerchris@hotmail.com>
2015-12-05 17:35:25 -0800

    models.py good to go, migrations made, db modified

--- rango/models.py
+++ rango/models.py
@@ -1,7 +1,5 @@
-from django.db import models
-
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    name = models.CharField(max_length=128, unique=True
 
     def __unicode__(self):
         return self.name
