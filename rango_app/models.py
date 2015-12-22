b426522513c572fa5d29338f6aeb4d154eaab5db
chrisheckler <hecklerchris@hotmail.com>
2015-12-21 23:27:36 -0800

    UserProfile added in admin.py

--- rango_app/models.py
+++ rango_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
