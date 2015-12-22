fdc5cb346162ced3d4375ed270b9914f2bba3106
chrisheckler <hecklerchris@hotmail.com>
2015-12-22 00:29:36 -0800

    added register() to  views.py

--- rango_app/forms.py
+++ rango_app/forms.py
@@ -1,5 +1,18 @@
 from django import forms
-from rango_app.models import Page, Category
+from django.contrib.auth.models import User
+from rango_app.models import Page, Category, UserProfiles
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfilesForm(forms.ModelForm):
+    class meta:
+        model = UserProfile
+        fields = ('website', 'picture')
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128, help_text="Please enter the category name.")
--- rango_app/views.py
+++ rango_app/views.py
@@ -4,6 +4,7 @@ from rango_app.models import Category, Page
 from rango_app.forms import CategoryForm
 from rango.views import *
 from rango_app.forms import PageForm
+from rango_app.forms import UserForm, UserProfileForm
 from django.template import RequestContext
 
 
@@ -13,6 +14,39 @@ def encode_url(str):
 def decode_url(str):
     return str.replace('_', ' ')
 
+def register(request):
+
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+
+            registered = True
+
+        else:
+            print user_form.errors, profile_form.errors
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request,
+            'rango/register.html',
+            {'user_form': user_form, 'profile_form': profile_form, 'registered': registered} )
 
 def get_category_list(max_results=0, starts_with=''):
     cat_list = []
